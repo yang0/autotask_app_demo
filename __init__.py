68f82ba6bbfbc9769639e029c4581be08d8cf8d7
yang0 <yang0@126.com>
2025-04-16 13:19:18 +0800

    'description'

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ from .demo import *
 
 VERSION = "0.0.1"
 GIT_URL = "https://github.com/yang0/autotask_app_demo.git"
-NAME = "File Browser Demo"
-DESCRIPTION = """A demo application for browsing files using Streamlit"""
+NAME = "Time Display Demo"
+DESCRIPTION = """A demo application for displaying current time in different formats and timezones using Streamlit"""
 
 
-TAGS = ["application", "demo", "streamlit", "file browser"]
+TAGS = ["application", "demo", "streamlit", "time", "timezone"]
